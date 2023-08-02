f1c179d8c44b0de8db810c2327218dd6642031d8
Sabilillah <sabilillah272@gmail.com>
2023-08-02 10:16:46 +0700

    Update order-controller.js

--- src/controllers/order-controller.js
+++ src/controllers/order-controller.js
@@ -16,7 +16,7 @@ const checkout = async (req, res, next) => {
   try {
     const request = {
       userId: req.user.id,
-      orderId: req.query.orderId,
+      orderId: req.query.id,
       username: req.user.username,
     };
 
