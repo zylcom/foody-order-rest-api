d1f77b47f442f8b49acc318abea7772de3d2f185
sabilillah <sabilillah272@gmail.com>
2023-11-23 18:32:35 +0700

    update web.js

--- src/app/web.js
+++ src/app/web.js
@@ -15,7 +15,6 @@ const options = {
   swaggerOptions: {
     url: "https://gist.githubusercontent.com/zylcom/090fb0e5b523832810f7da8a9ba87f2f/raw/c0587de1830a2c965199172d997d1bf424803945/foody-order-api-spec.json",
   },
-  customCssUrl: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.6.2/swagger-ui.min.css",
 };
 
 web.use(cors(corsOptions));
