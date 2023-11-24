2ec1470f43fa6c6afb91d0776dc6075d0e49c0c5
sabilillah <sabilillah272@gmail.com>
2023-11-24 14:34:09 +0700

    update web.js

--- src/app/web.js
+++ src/app/web.js
@@ -16,13 +16,13 @@ const options = {
   swaggerOptions: {
     url: "https://gist.githubusercontent.com/zylcom/090fb0e5b523832810f7da8a9ba87f2f/raw/c0587de1830a2c965199172d997d1bf424803945/foody-order-api-spec.json",
   },
-  customCssUrl: path.join(process.cwd(), "public", "api-docs", "swagger-ui.css"),
+  customCssUrl: "/api-docs/swagger-ui.css",
 };
 
 web.use(cors(corsOptions));
 web.use(webhookRouter);
 web.use(express.json());
-web.use(express.static(path.join(process.cwd(), "public")));
+web.use(express.static(path.resolve("./public")));
 web.use("/api/docs", express.static("node_modules/swagger-ui-dist/", { index: false }), swaggerUi.serve, swaggerUi.setup(null, options));
 web.use(publicRouter);
 web.use(userRouter);
