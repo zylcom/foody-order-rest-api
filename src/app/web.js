1aa44d35353e7d05b6a5b34d83ccd682efa76390
sabilillah <sabilillah272@gmail.com>
2023-11-24 11:13:04 +0700

    update web.js

--- src/app/web.js
+++ src/app/web.js
@@ -5,6 +5,7 @@ import { errorMiddleware } from "../middleware/error-middleware.js";
 import { publicRouter } from "../routes/public-api.js";
 import { userRouter } from "../routes/api.js";
 import { webhookRouter } from "../routes/webhook.js";
+import path from "path";
 
 export const web = express();
 const corsOptions = {
@@ -15,13 +16,13 @@ const options = {
   swaggerOptions: {
     url: "https://gist.githubusercontent.com/zylcom/090fb0e5b523832810f7da8a9ba87f2f/raw/c0587de1830a2c965199172d997d1bf424803945/foody-order-api-spec.json",
   },
-  customCssUrl: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.7.0/swagger-ui.min.css",
 };
 
 web.use(cors(corsOptions));
 web.use(webhookRouter);
 web.use(express.json());
-web.use("/api/docs", swaggerUi.serve, swaggerUi.setup(null, options));
+web.use(express.static(path.join(process.cwd(), "public")));
+web.use("/api/docs", express.static("node_modules/swagger-ui-dist/", { index: false }), swaggerUi.serve, swaggerUi.setup(null, options));
 web.use(publicRouter);
 web.use(userRouter);
 web.use(errorMiddleware);
