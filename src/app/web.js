43ab102ebe2ac740746b9153bcb01a54124829cc
sabilillah <sabilillah272@gmail.com>
2023-11-25 12:38:49 +0700

    docs: change documentation spec from json to yaml

--- src/app/web.js
+++ src/app/web.js
@@ -1,21 +1,23 @@
-import express from "express";
 import cors from "cors";
+import express from "express";
+import fs from "fs";
+import path from "path";
 import swaggerUi from "swagger-ui-express";
+import YAML from "yaml";
 import { errorMiddleware } from "../middleware/error-middleware.js";
 import { publicRouter } from "../routes/public-api.js";
 import { userRouter } from "../routes/api.js";
 import { webhookRouter } from "../routes/webhook.js";
-import path from "path";
 
 export const web = express();
 const corsOptions = {
   origin: "*",
 };
 
+const file = fs.readFileSync("./src/docs/api-spec.yaml", "utf8");
+const swaggerDocument = YAML.parse(file);
+
 const options = {
-  swaggerOptions: {
-    url: "/api-docs/api-spec.json",
-  },
   customCssUrl: "/api-docs/swagger-ui.css",
 };
 
@@ -30,7 +32,7 @@ web.use((err, req, res, next) => {
   next();
 });
 web.use(express.static(path.resolve("./public")));
-web.use("/api/docs", express.static("node_modules/swagger-ui-dist/", { index: false }), swaggerUi.serve, swaggerUi.setup(null, options));
+web.use("/api/docs", express.static("node_modules/swagger-ui-dist/", { index: false }), swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 web.use(publicRouter);
 web.use(userRouter);
 web.use(errorMiddleware);
