904b705d5f089068106d6c6c47b05fafa51bca5b
sabilillah <sabilillah272@gmail.com>
2023-12-02 16:55:41 +0700

    update like product api

--- src/services/like-product-service.js
+++ src/services/like-product-service.js
@@ -8,14 +8,11 @@ const like = async (request) => {
   request = validate(likeValidation, request);
 
   const countLike = await prismaClient.likeOnProduct.count({ where: { AND: [{ username: request.username }, { productSlug: request.productSlug }] } });
-  const product = await productService.get(request.productSlug);
+
+  await productService.get(request.productSlug);
 
   if (countLike > 0) {
-    throw new ResponseError(400, "Product is already liked!");
-  }
-
-  if (!product) {
-    throw new ResponseError(404, "Product not found!");
+    throw new ResponseError(409, "Product is already liked!");
   }
 
   return prismaClient.likeOnProduct.create({ data: { user: { connect: { username: request.username } }, product: { connect: { slug: request.productSlug } } } });
@@ -25,14 +22,11 @@ const neutral = async (request) => {
   request = validate(likeValidation, request);
 
   const countLike = await prismaClient.likeOnProduct.count({ where: { AND: [{ username: request.username }, { productSlug: request.productSlug }] } });
-  const product = await productService.get(request.productSlug);
+
+  await productService.get(request.productSlug);
 
   if (countLike !== 1) {
     throw new ResponseError(400, "Product is not liked!");
-  }
-
-  if (!product) {
-    throw new ResponseError(404, "Product not found!");
   }
 
   return prismaClient.likeOnProduct.delete({ where: { like: { productSlug: request.productSlug, username: request.username } } });
